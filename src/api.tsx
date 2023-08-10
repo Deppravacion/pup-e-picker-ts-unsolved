d9e05f766a664b001756edec896c06ec0292d4ca
Tyson <deppravacion@gmail.com>
2023-08-10 12:03:35 -0600

    wrapped

--- src/api.tsx
+++ src/api.tsx
@@ -4,18 +4,18 @@ import { Dog } from "./types";
 export const baseUrl = "http://localhost:3000";
 
 export const Requests = {
-  getAllDogs: async (): Promise<Dog[]> => {        
-    return await fetch(`${baseUrl}/dogs`).then(res => res.json())  
-  }, 
-  postDog: async (dog: Omit<Dog, 'id'>) => { 
+  getAllDogs: async (): Promise<Dog[]> => {
+    return await fetch(`${baseUrl}/dogs`).then(res => res.json())
+  },
+  postDog: async (dog: Omit<Dog, 'id'>) => {
     try {
       const res = await fetch(`${baseUrl}/dogs`, {
-        method: 'POST', 
+        method: 'POST',
         body: JSON.stringify(dog),
         headers: {
           "Content-Type": "application/json"
         }
-      })     
+      })
       await res.json()
       if (!res.ok) {
         throw new Error('could not POST that dog')
@@ -25,9 +25,9 @@ export const Requests = {
     } catch (error) {
       toast.error('oops there was a problem')
       console.error(error)
-    }    
+    }
   },
-  deleteDog: async (id: number) => { 
+  deleteDog: async (id: number) => {
     try {
       const res = await fetch(`${baseUrl}/dogs/${id}`, {
         method: "DELETE"
@@ -44,13 +44,13 @@ export const Requests = {
     }
   },
 
-  updateDog: async (isFavorite: boolean , id: number) => {
-  // updateDog: async (dog: Partial<Dog> , id: number) => {
+  updateDog: async (isFavorite: boolean, id: number) => {
+    // updateDog: async (dog: Partial<Dog> , id: number) => {
     const fav = isFavorite ? false : true
     try {
       const res = await fetch(`${baseUrl}/dogs/${id}`, {
-        method: 'PATCH', 
-        body: JSON.stringify({isFavorite: fav}),
+        method: 'PATCH',
+        body: JSON.stringify({ isFavorite: fav }),
         // body: JSON.stringify(dog),
         headers: {
           "Content-Type": "application/json"
